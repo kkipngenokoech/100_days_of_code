03bed817a3ff64f53e631bb3ab8217f01db30bf9
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-09 22:42:49 +0300

    updated the settings.py template directory

--- DJANGO/newpages/pages/settings.py
+++ DJANGO/newpages/pages/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pageapp.apps.PageappConfig'#first installed app
+    'pageapp.apps.PageappConfig',#first installed app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'pages.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
