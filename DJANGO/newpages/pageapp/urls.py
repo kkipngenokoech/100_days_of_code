1c18a3cec4b43c9829eab3d239f1a16c8880e5d4
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-10 13:59:38 +0300

    configuration of class based views templates

---
+++ DJANGO/newpages/pageapp/urls.py
@@ -0,0 +1,8 @@
+#app-level urls.py
+from django.urls import path
+from .views import HomePageView
+
+
+urlpatterns = [
+    path('',HomePageView.as_view(),name='home')#.as_view()- is a class based view
+]
