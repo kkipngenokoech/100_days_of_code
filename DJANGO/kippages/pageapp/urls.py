1d9dd2e8a994c577d4748917371a4271024375f9
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-11 13:58:35 +0300

    added the new url route

--- DJANGO/kippages/pageapp/urls.py
+++ DJANGO/kippages/pageapp/urls.py
@@ -1,6 +1,8 @@
 from re import template
+from unicodedata import name
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, AboutPageView
 urlpatterns = [
-    path('',HomePageView.as_view(template_name="home.html"),name='home'),
+    path('about/',AboutPageView.as_view(template_name="about.html",name='about')),
+    path('',HomePageView.as_view(template_name="home.html"),name='home')
 ]
