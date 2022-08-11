743d3c1ab7aba2fd8cdded902425f81e469490c0
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-11 14:21:01 +0300

    tested and verified, the about page is working well

--- DJANGO/kippages/pageapp/urls.py
+++ DJANGO/kippages/pageapp/urls.py
@@ -3,6 +3,6 @@ from unicodedata import name
 from django.urls import path
 from .views import HomePageView, AboutPageView
 urlpatterns = [
-    path('about/',AboutPageView.as_view(template_name="about.html",name='about')),
+    path('about/',AboutPageView.as_view(template_name="about.html"),name='about'),
     path('',HomePageView.as_view(template_name="home.html"),name='home')
 ]
