1c39e309180ed6123e4a5e4576ff039e251db0cd
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-15 16:21:06 +0300

    start of a new project - message app

---
+++ DJANGO/message_board_app/message_app_project/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for message_app_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'message_app_project.settings')
+
+application = get_wsgi_application()
