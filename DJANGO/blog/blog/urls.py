f1761ea0f33a89a74b777531c214cd1b5d904196
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-16 21:15:51 +0300

    up until now my django blog app is working perfectly fine, I have done alot today

--- DJANGO/blog/blog/urls.py
+++ DJANGO/blog/blog/urls.py
@@ -2,6 +2,7 @@ from importlib.resources import path
 from unicodedata import name
 from django.contrib import admin
 from .views import BlogListView
-urlspattern = [
-    path('',BlogListView.as_view(),name = 'home')
+from django.urls import path
+urlpatterns = [
+    path('',BlogListView.as_view(template_name = 'home.html'),name='home')
 ]
