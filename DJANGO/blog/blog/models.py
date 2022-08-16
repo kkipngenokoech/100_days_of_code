e4ea55d7af3959032cca22a2fe130f7c9b14e40b
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-16 19:06:54 +0300

    created a database model for our django app

--- DJANGO/blog/blog/models.py
+++ DJANGO/blog/blog/models.py
@@ -1,3 +1,12 @@
+from pyexpat import model
+from turtle import title
 from django.db import models
 
-# Create your models here.
+# Create your models here
+#here we are creating a subclass of models.model
+class post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.user',on_delete=models.CASCADE)
+    body = models.TextField()
+    def __str__(self):
+        return self.title
