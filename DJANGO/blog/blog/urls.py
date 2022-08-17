31c274cfe50be01c20418a50dec5080f37a7e624
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-17 21:48:23 +0300

    views -> urls -> templates

--- DJANGO/blog/blog/urls.py
+++ DJANGO/blog/blog/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from .views import BlogListView,BlogDetailView
 from django.urls import path
 urlpatterns = [
-    path('post/<int:pk>/',BlogDetailView.as_view(template_name = 'post_detail.html'),name="post_detail"),
+    path('post/<int:pk>/',BlogDetailView.as_view(),name="post_detail"),
     path('',BlogListView.as_view(template_name = 'home.html'),name='home')
 ]
