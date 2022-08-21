6cb00fa5e2f7230699ad9efbcb9c22b7ef6267ba
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-21 18:14:27 +0300

    updated the list of installed apps, and linked the project level and app levelurls and the view

---
+++ CustomLogin/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path("register",views.register,name="register"),
+    path("login_user",views.login_user,name="login_user"),
+    path("logout_user",views.logout_user,name="logout_user"),
+    path("home",views.home,name="home")
+]
