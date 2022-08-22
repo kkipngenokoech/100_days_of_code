b9707c904c56c8b6182d6dba0bf10a1b820ddd2a
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-22 20:32:12 +0300

    add a url reruote for our newly created BlogUpdateView template

--- DJANGO/blog/blog/urls.py
+++ DJANGO/blog/blog/urls.py
@@ -1,9 +1,10 @@
 from importlib.resources import path
 from unicodedata import name
 from django.contrib import admin
-from .views import BlogListView,BlogDetailView, BlogCreateView
+from .views import BlogListView,BlogDetailView, BlogCreateView, BlogUpdateView
 from django.urls import path
 urlpatterns = [
+    path('post/<int:pk>/',BlogUpdateView.as_view(),name = 'post_edit'),
     path('post/new/',BlogCreateView.as_view(),name = 'post_new'),
     path('post/<int:pk>/',BlogDetailView.as_view(),name="post_detail"),
     path('',BlogListView.as_view(template_name = 'home.html'),name='home')
