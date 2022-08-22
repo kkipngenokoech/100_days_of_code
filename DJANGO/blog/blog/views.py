922e63521ddd2ccb226fce458039bdb8d030b315
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-22 20:53:05 +0300

    created a view for deleting our post

--- DJANGO/blog/blog/views.py
+++ DJANGO/blog/blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 # we are going to use listview and the model we created
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import post
+from django.urls import reverse_lazy
 
 # Create your views here.
 class BlogListView(ListView):
@@ -22,3 +23,8 @@ class BlogUpdateView(UpdateView):
     model = post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')# reverse_lazy won't redirect our url until the post has finished deleting
