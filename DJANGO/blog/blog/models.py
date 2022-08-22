7ebf3d9a4b0725c340b89d75f9441b95003ad02e
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-22 20:16:44 +0300

    adding the feature of being able to edit our blog

--- DJANGO/blog/blog/models.py
+++ DJANGO/blog/blog/models.py
@@ -1,6 +1,7 @@
 from pyexpat import model
 #from turtle import title
 from django.db import models
+from  django.urls import reverse
 
 # Create your models here
 #here we are creating a subclass of models.model
@@ -10,3 +11,6 @@ class post(models.Model):
     body = models.TextField()
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail',args=[str(self.id)])
