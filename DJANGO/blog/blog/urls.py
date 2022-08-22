c5623bec4d88b9ba003ac7abc4fee1e13f51fbd0
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-22 20:58:05 +0300

    added a url reroute for our deleteview

--- DJANGO/blog/blog/urls.py
+++ DJANGO/blog/blog/urls.py
@@ -1,9 +1,10 @@
 from importlib.resources import path
 from unicodedata import name
 from django.contrib import admin
-from .views import BlogListView,BlogDetailView, BlogCreateView, BlogUpdateView
+from .views import BlogListView,BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
 from django.urls import path
 urlpatterns = [
+    path('post/<int:pk>/',BlogDeleteView.as_view(),name="post_delete"),
     path('post/<int:pk>/',BlogUpdateView.as_view(),name = 'post_edit'),
     path('post/new/',BlogCreateView.as_view(),name = 'post_new'),
     path('post/<int:pk>/',BlogDetailView.as_view(),name="post_detail"),
