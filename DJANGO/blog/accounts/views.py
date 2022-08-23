ad810037b701ba2881b4c383a73c1d994a766a5c
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-23 21:46:55 +0300

    we are first served the signup page then redirected to the login page then redirected to the home page

--- DJANGO/blog/accounts/views.py
+++ DJANGO/blog/accounts/views.py
@@ -6,5 +6,5 @@ from django.views import generic
 
 class SignUpView(generic.CreateView):
     form_class = UserChangeForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('login')# it serves the login upon success
     template_name = 'signup.html'
