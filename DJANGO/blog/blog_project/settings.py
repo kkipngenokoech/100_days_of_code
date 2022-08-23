adc762f441a5bae4f7557feb6da09068363278aa
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-23 20:27:37 +0300

    added a login redirect for the user once login is successful

--- DJANGO/blog/blog_project/settings.py
+++ DJANGO/blog/blog_project/settings.py
@@ -122,3 +122,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = 'home'
