eb4b28e5bd1fb62ce2a58301701880034b44e04f
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-24 16:39:50 +0300

    created a forms.py and updated our customuser creation form

---
+++ DJANGO/news/users/forms.py
@@ -0,0 +1,11 @@
+from dataclasses import field
+from pyexpat import model
+from django import forms
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import CustomUser
+
+# our CustomUser contains all the default fields and our newly added age.
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
