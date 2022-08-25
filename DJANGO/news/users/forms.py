a218db9d6e1da05a0695406e5fd722e326d9fe4b
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-25 13:35:22 +0300

    testing of my customer signup , logging in and logging out

--- DJANGO/news/users/forms.py
+++ DJANGO/news/users/forms.py
@@ -8,9 +8,9 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username','email','age')
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ('username','email','age')
