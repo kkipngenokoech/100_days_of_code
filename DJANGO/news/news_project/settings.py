f2a12ee8fb023bbb2f2d4a0a838a0c944e366f56
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-29 07:16:54 +0300

    updated the bootstrap version in my settings py

--- DJANGO/news/news_project/settings.py
+++ DJANGO/news/news_project/settings.py
@@ -132,5 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
