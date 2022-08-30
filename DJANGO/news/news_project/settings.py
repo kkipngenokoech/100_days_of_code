2a04d1aa9ad2ca73f53a1c1ffbe54ead4de8c7a1
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-30 06:03:37 +0300

    configuration of sendrid password

--- DJANGO/news/news_project/settings.py
+++ DJANGO/news/news_project/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+from dotenv import load_dotenv
+load_dotenv()
 from pathlib import Path
 import os
 from pickle import TRUE
@@ -140,6 +141,6 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' # - sends it via smtp
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = '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'
+EMAIL_HOST_PASSWORD = os.environ.get("SENDGRID_API_KEY")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
