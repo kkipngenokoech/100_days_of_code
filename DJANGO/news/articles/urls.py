a47688280ef80fb305e311be5d0d3357b95014a2
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-31 20:12:42 +0300

    updated my article_list.html edit and delete with primary keys

--- DJANGO/news/articles/urls.py
+++ DJANGO/news/articles/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import ArticleListView, ArticleUpdateView,ArticleDetailView, ArticleDeleteView
 
 urlpatterns = [
+    path('<int:pk>/edit/',ArticleUpdateView.as_view(), name='article_edit'),
+    path('<int:pk>/delete/',ArticleDeleteView.as_view(), name='article_delete'),
+    path('<int:pk>/',ArticleDetailView.as_view(), name='article_detail'),
     path('', ArticleListView.as_view(), name='article_list'),
 ]
