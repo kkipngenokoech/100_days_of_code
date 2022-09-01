3b499d900ca78dce44b91a3d7366d644a59935db
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-09-01 09:06:31 +0300

    made correction on the editing url, view, and templates

--- DJANGO/news/news_project/urls.py
+++ DJANGO/news/news_project/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/',include('users.urls')),
     path('users/',include('django.contrib.auth.urls')),
-    path('articles',include('articles.urls')),
+    path('articles/',include('articles.urls')),
     #path('',TemplateView.as_view(template_name = 'home.html'),name='home')
     path('',include('pages.urls'))
 ]
