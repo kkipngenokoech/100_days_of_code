20031cf3d6cd5d998ee12e702dae74f8e947db47
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-09-09 13:26:57 +0300

    called the component user into another component main

--- REACT/30-days-of-react/src/index.js
+++ REACT/30-days-of-react/src/index.js
@@ -5,6 +5,11 @@ import kipUser from './images/bugbunny.jpg'
 const jsx_element = <h1>I am a JSX element</h1>
 const root_element = document.getElementById('root')
 const title = <h2>this is me learning 30 days of react</h2>
+const user = (
+  <div>
+    <img src={kipUser} alt="kipngenokoech" />
+  </div>
+)
 const header = (
   <header>
     <h1>suits</h1>
@@ -15,6 +20,7 @@ const main = (
   <main>
       <p>its  main character is Harvey specter</p>
       <p>location: Newyork city </p>
+      {user}
   </main>
 )
 const footer = (
@@ -22,17 +28,13 @@ const footer = (
       <p>copywright: kkipngenokoech</p>
   </footer>
 )
-const user = (
-  <div>
-    <img src={kipUser} alt="kipngenokoech" />
-  </div>
-)
+
 const app = (
   <div>
       {header}
       {title}
       {jsx_element}
-      {user}
+      {/* {user} */}
       {main}
       {footer}
   </div>
