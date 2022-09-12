7777e82b67b7825931f43422f7097eca96914b4b
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-09-12 22:12:18 +0300

    rendered my first object

--- REACT/state/src/App.js
+++ REACT/state/src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react'
+import "./App.css"
+class App extends React.Component {
+  render(){
+    return (
+    <div className = "App">
+      <h1>welcome to multiverse</h1>
     </div>
-  );
+    )
+  }
 }
-
 export default App;
