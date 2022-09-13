249e63d7d86dcce1a402e02b9709bae8322fe0bc
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-09-13 09:12:25 +0300

    added a boiler plate our new component

---
+++ REACT/state/src/components/new_component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+export class new_component extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+export default new_component
