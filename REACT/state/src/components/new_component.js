08df8847dd06bfe23ad4536f54a90bb6ff3ed993
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-09-13 09:24:06 +0300

    i have created the first return action

--- REACT/state/src/components/new_component.js
+++ REACT/state/src/components/new_component.js
@@ -1,9 +1,17 @@
 import React, {Component} from 'react'
 export class new_component extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         message : "subscribe to my multiverse"
+      }
+    }
+    
     render() {
         return (
-            <div>
-
+            <div className='App'>
+                <h1>{this.state.message}</h1>
             </div>
         )
     }
