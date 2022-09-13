c3ba488f181ccd4653edce70ad89314b37cc92f1
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-09-13 09:04:09 +0300

    styling my render element

--- REACT/state/src/App.js
+++ REACT/state/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import "./App.css"
 class App extends React.Component {
+  styles = {
+    fontStyle : "bold",
+    color : "teal"
+  }
   render(){
     return (
     <div className = "App">
-      <h1>welcome to multiverse</h1>
+      <h1 style={this.styles}>welcome to multiverse</h1>
     </div>
     )
   }
