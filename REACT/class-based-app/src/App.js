95143836a7ca950a0d533512263c264774389dc9
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-09-16 08:20:27 +0300

    using props and classes with states

--- REACT/class-based-app/src/App.js
+++ REACT/class-based-app/src/App.js
@@ -1,5 +1,21 @@
 import React from 'react'
 
+class Team extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {change : true}
+  }
+  render () {
+    return (
+      <div>
+        <button onClick={() => {this.setState({change : !this.state.change})}}>Choose Team!</button>
+        {
+          this.state.change?(<h1>welcome to the Women's Team</h1>):(<h1>welcome to the Men's Team</h1>)
+        }
+      </div>
+    )
+  }
+}
 class Player extends React.Component {
   render () {
     return <h1>Here's data about players</h1>
@@ -10,7 +26,8 @@ class App extends React.Component {
     return (
     <div>
       <h1>Chelsea Football Club</h1>
-      <Player/>
+      <Player />
+      <Team />
     </div>
     )
   }
