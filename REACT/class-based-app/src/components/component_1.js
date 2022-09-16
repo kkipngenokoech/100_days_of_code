f4045b78382cbeef58eb574cb290770d59d7b315
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-09-16 20:38:52 +0300

    created an class-based js

---
+++ REACT/class-based-app/src/components/component_1.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class MyComponent extends Component {
+    render () {
+        return (
+            <div>
+                <h1>This is a sample of component place on an individual file</h1>
+            </div>
+        )
+    }
+}
