ca590575c5d7ebe40e206e5d27193884400872a7
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-09-19 21:23:53 +0300

    created a list of options

---
+++ REACT/forms/src/components/diffrentinputs.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const options = [
+    {value: '',label : '--select Country--'},
+    {value : 'Finland',label : 'Finland'},
+    {value : 'Sweden', label : 'Sweden'},
+    {value : 'Norway', label : 'Norway'},
+    {value : 'Denmark', label : 'Denmark'}
+]
+class DiffrentInput extends React.Component {
+}
+export default DiffrentInput
