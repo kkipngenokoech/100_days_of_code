5fc88b56973c299cfd9a4f931d0d34acb41ebd30
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-09-23 20:34:54 +0300

    added logic for removing items, it filters out the one with id

--- REACT/shopping-list/src/components/items.js
+++ REACT/shopping-list/src/components/items.js
@@ -3,5 +3,7 @@ import '../styles/item.css'
 
 //functional way of creating a component.
 const item = ({id, items, list, setList, complete}) => {
-
+    const remove = (id) => {
+        setList(list.filter((el) => el.id !== id))
+    }
 }
