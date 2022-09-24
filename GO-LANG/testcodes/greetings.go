82e0a28c2850ab55c184ef41a15bc2b4d9bb34de
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-09-24 21:53:54 +0300

    trimming white spaces

--- GO-LANG/testcodes/greetings.go
+++ GO-LANG/testcodes/greetings.go
@@ -1,10 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, enter your name:")
 	var username string
 	fmt.Scanln(&username)
+	username = strings.TrimSpace(username)
 	fmt.Printf("Hi, %s!, I am go languange", username)
 }
