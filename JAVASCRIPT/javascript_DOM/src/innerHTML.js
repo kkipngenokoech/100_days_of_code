0bce3e8443ca90a20bb23cb6bd9cdb18d37e0a3a
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-09-30 21:11:31 +0300

    innerHTML generating data on click

--- JAVASCRIPT/javascript_DOM/src/innerHTML.js
+++ JAVASCRIPT/javascript_DOM/src/innerHTML.js
@@ -1,4 +1,15 @@
+let flag = true
 const comment_generator_field = () => {
-    let create_data = "Name: <input type = 'text' name = 'name'> <br> <textarea rows = '5' cols = '80'></textarea> <br> <input type = 'submit' value = 'post comment'"
-    document.getElementById('myLocation').innerHTML = create_data
+   // let create_data = "Name: <input type = 'text' name = 'name'> <br> <textarea rows = '5' cols = '80'></textarea> <br> <input type = 'submit' value = 'post comment'"
+   let form_data = "<form action = 'comment'>Enter Name: <br><input type = 'text' name = 'name'/></br>Enter Email:<br><input type = 'email' name = 'email'></input></br><br>Enter comment:</br><textarea rows = '5' cols = '70'></textarea><br><input type = 'submit' value = 'post comment'></form>"
+   if (flag)
+   {
+    document.getElementById('myLocation').innerHTML = form_data
+    flag = false
+   }
+   else
+   {
+    document.getElementById('myLocation').innerHTML = ""
+    flag = true
+   }
 }
