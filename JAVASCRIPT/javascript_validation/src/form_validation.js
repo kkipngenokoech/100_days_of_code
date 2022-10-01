ec5402c5765f13e5a3cf66472000184b07bad66e
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-10-01 08:17:38 +0300

    form validation using js

--- JAVASCRIPT/javascript_validation/src/form_validation.js
+++ JAVASCRIPT/javascript_validation/src/form_validation.js
@@ -1,6 +1,7 @@
 const form_validate = () => {
     let user_name = document.validate_form.username.value
-    let user_password = document.getElementByName('user_password').value
+    let user_password = document.getElementById('user_password').value
+    let reenter_password = document.getElementById('reenter_password').value
     if (user_name == null || user_name == "")
     {
         alert(`username cannot be blank`)
@@ -9,6 +10,11 @@ const form_validate = () => {
     else if (user_password.length < 6)
     {
         alert(`password must be at least 6 characters long`)
+        return false
+    }
+    else if (user_password != reenter_password)
+    {
+        alert(`passwords must be the same`)
         return false
     }
     else
