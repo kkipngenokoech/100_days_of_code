103aceb2de338a20b92de873cf9d11b9a95ebceb
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-10-06 22:05:57 +0300

    using time library in ruby

--- RUBY/tutorials/ruby_codes/datetime.rb
+++ RUBY/tutorials/ruby_codes/datetime.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/ruby -w
 time_now = Time.new
-puts "current time is: #{time_now}"
+puts "current time is: #{time_now.inspect}"
 
 # you can use both new and now.
 
 time_now = Time.now
-print "current time is: " << "#{time_now}\n"
+print "current time is: " << "#{time_now.inspect}\n"
