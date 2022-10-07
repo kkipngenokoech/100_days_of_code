660853e0ced6c97ee42734eb94e5593c34309bde
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-10-07 20:03:56 +0300

    ternay operator

--- RUBY/tutorials/conditional.md
+++ RUBY/tutorials/conditional.md
@@ -1,7 +1,8 @@
-# if statement
+# [if statement](./ruby_codes/conditionals.rb)
 this is used to test conditionals.
 the statement syntaxs used include:
     if statement
     if-else statement
     if -else if -statement
     ternary operators.
+ternary operators - this is a way of shorthanding ruby  if else statements.s
--- RUBY/tutorials/ruby_codes/conditionals.rb
+++ RUBY/tutorials/ruby_codes/conditionals.rb
@@ -5,6 +5,9 @@ if index_age >= 18
     puts 'you are eligible to vote'
 elsif index_age >= 16 && index_age <= 18
     print "current age is just a little short for voting\n"
+    puts 'will you like to vote in the next elections when you qualify?'
+    adult_choice = gets.chomp.downcase
+    puts "#{adult_choice} == "yes"?"thats a champ":"that's sad to hear"#
 else 
     puts 'you are not eligible to vote'
 end
