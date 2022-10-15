b47324ee30b01709e9449d995730eb1dca888942
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-10-15 08:14:15 +0300

    my code is working with single words passed in, its fit

--- RUBY/Ruby_Projects/Sub_strings/README.md
+++ RUBY/Ruby_Projects/Sub_strings/README.md
@@ -2,3 +2,6 @@ welcome to the substrings project
 
 ## substring
 takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument. It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found.
+
+## method substring
+it iterates over the string as it checks if it finds a match of the string.
--- RUBY/Ruby_Projects/Sub_strings/sub_strings.rb
+++ RUBY/Ruby_Projects/Sub_strings/sub_strings.rb
@@ -1,14 +1,21 @@
 #!usr/bin/ruby
 
 def sub_string(string, dictionary)
-    for index in 0..string.length - 1
-        if (dictionary.include? string[0..index])
-            puts "yes = #{string[0..index]}"
+    ## empty hash
+    appearance = {}
+    dictionary.each do |element|
+        if (string.include? element)
+            if (appearance.has_key?(element))
+                appearance[element] += 1
+            else
+                appearance[element] = 1
+            end
         end
     end
+    puts appearance
 
 end
 
 # test data
- dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+ dictionary = ["below","below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
  sub_string("below", dictionary)
