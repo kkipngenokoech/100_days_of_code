24197536cc233ab1b8e2cb0873ab98e54def5fa4
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-10-17 11:36:40 +0300

    appending zeros in zipcodes

--- RUBY/Ruby_Projects/event_manager/lib/eventRubycsv.rb
+++ RUBY/Ruby_Projects/event_manager/lib/eventRubycsv.rb
@@ -1,14 +1,7 @@
 #!usr/bin/ruby
 require "csv"
-puts "eventManager initialized....."
 
-eventContents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
-
-# accessing first name for attendees
-eventContents.each do |row|
-    state = row[-2]
-    name = row[:first_name]
-    zipcode = row[-1]
+def cleanZipcodes (zipcode)
     zipcode.to_s
     if zipcode.nil?
         zipcode = '00000'
@@ -22,12 +15,26 @@ eventContents.each do |row|
         zipcode = newzip + zipcode
     elsif zipcode.length > 5
         zipcode = zipcode[0..4]
+    else
+        zipcode
     end
+end
+
+puts "eventManager initialized....."
+
+eventContents = CSV.open('event_attendees.csv', headers: true, header_converters: :symbol)
+
+# accessing first name for attendees
+eventContents.each do |row|
+    state = row[-2]
+    name = row[:first_name]
+    zipcode = row[-1]
     # if zipcode.length < 5
     #     zipcode =zipcode.rjust(5,'0')
     # elsif zipcode.length > 5
     #     zipcode = zipcode[0..4]
     # end
+    zipcode = cleanZipcodes(zipcode)
 
     puts "#{name}, #{state}, #{zipcode}"
 end
