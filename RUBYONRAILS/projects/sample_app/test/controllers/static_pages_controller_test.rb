7cabc431f84ea510ed837c59df21dfec72bccc9d
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-10-31 23:00:43 +0300

    adding custom made titles

--- RUBYONRAILS/projects/sample_app/test/controllers/static_pages_controller_test.rb
+++ RUBYONRAILS/projects/sample_app/test/controllers/static_pages_controller_test.rb
@@ -4,15 +4,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | rails sample app"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "help | rails sample app"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | rails sample app"
   end
 end
