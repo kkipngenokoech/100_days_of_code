e20fb89b336e76cc9c3b1c06c2ed5ef5b053ef28
kkipngenokoech <kkipngenokoech22@gmail.com>
2023-02-05 22:53:21 +0300

    routes

--- DJANGO/news/pages/views.py
+++ DJANGO/news/pages/views.py
@@ -4,3 +4,4 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+    
--- RUBYONRAILS/projects/art_app/config/routes.rb
+++ RUBYONRAILS/projects/art_app/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :paintings, only: [:index, :show, :new, :create, :destoy]
+  resources :paintings, only: [:index, :show, :new, :create, :destroy]
   resources :galleries
   resources :artists # this gives us access to all the 7 restful routes. from here you head to the artist controllers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
